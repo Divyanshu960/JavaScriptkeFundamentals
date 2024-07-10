e788c54dec09d729af2346eac648de0930bc49e3
Divyanshu Chaurasiya <72182690+Divyanshu960@users.noreply.github.com>
2024-07-10 11:05:03 +0000

    new variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 20011255
+let accountEmail = "cdivyanshu10@gmail.com"
+var accountPass = "12345"
+accountCity = "Dehradun"
+
+// accountId = 2
+accountEmail= "cdivz@gmail.com"
+accountPass = 123123
+accountCity = "Lucknow"
+let accountState;
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
